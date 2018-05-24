1fba16c5ce15bdd7bb1f0fc84372896780be115e
Pavel Vaskou <3635019@gmail.com>
2018-05-24 17:26:09 +0300

    Boxing class for binding

---
+++ CarChat/CarChat/Model/Service/Box.swift
@@ -0,0 +1,31 @@
+//
+//  Box.swift
+//  CarChat
+//
+//  Created by Pavel Vaskou on 5/24/18.
+//  Copyright © 2018 Pavel Vaskou. All rights reserved.
+//
+
+import Foundation
+
+class Box<T> {
+    typealias Listener = (T) -> ()
+    
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    func bind(listener: @escaping Listener) {
+        self.listener = listener
+        listener(value)
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
